a0dffe67659ada34379a4e60cb5a94db0181c1a3
fj <fj@Viajero>
2022-01-24 00:21:16 +0900

    fix xwidget-webkit leaf block

--- init.el
+++ init.el
@@ -2401,66 +2401,56 @@ If setting prefix args (C-u), reuses session(buffer). Normaly session(buffer) cr
   ;; 				   (get-buffer (or prev "*scratch*"))))
   ;;     ))
   ;; (add-hook 'switch-buffer-functions #'switch-buffer-functions-func)
-  )
-
-(add-hook
- 'xwidget-webkit-mode-hook
- 'xwidget-webkit-mode-hook-func)
-(defun xwidget-webkit-mode-hook-func ()
-  (remove-hook
-   'kill-buffer-query-functions
-   #'xwidget-kill-buffer-query-function)
-  (local-set-key
-   (kbd "C-f")
-   'xwidget-webkit-forward)
-  (local-set-key
-   (kbd "C-b")
-   'xwidget-webkit-back)
-  (local-set-key
-   (kbd "C-r")
-   'xwidget-webkit-reload)
-  (local-set-key
-   (kbd "C-l")
-   'xwidget-webkit-browse-url)
-  (local-set-key
-   (kbd "C-w")
-   'xwidget-webkit-current-url-message-kill)
-  (local-set-key
-   (kbd "C-q")
-   'kill-current-buffer)
-  (local-unset-key (kbd "C-x 2"))
-  (local-unset-key (kbd "C-x 3")))
-
-(defvar xwidget-webkit-search-word-prefix "https://duckduckgo.com/?q=" 
-  "Search engine url and search request that before searching word.")
-(defvar xwidget-webkit-search-word-suffix "&kp=-1&kl=jp-jp" 
-  "Search engine option parameter that after searching word.")
-(defun xwidget-webkit-search-word (arg word)
-  "Searching from `xwidget-webkit-search-word-prefix' search engine.
+  (add-hook
+   'xwidget-webkit-mode-hook
+   'xwidget-webkit-mode-hook-func)
+  (defun xwidget-webkit-mode-hook-func ()
+    (remove-hook
+     'kill-buffer-query-functions
+     #'xwidget-kill-buffer-query-function)
+    (local-set-key
+     (kbd "C-f")
+     'xwidget-webkit-forward)
+    (local-set-key
+     (kbd "C-b")
+     'xwidget-webkit-back)
+    (local-set-key
+     (kbd "C-r")
+     'xwidget-webkit-reload)
+    (local-set-key
+     (kbd "C-l")
+     'xwidget-webkit-browse-url)
+    (local-set-key
+     (kbd "C-w")
+     'xwidget-webkit-current-url-message-kill)
+    (local-set-key
+     (kbd "C-q")
+     'kill-current-buffer)
+    (local-unset-key (kbd "C-x 2"))
+    (local-unset-key (kbd "C-x 3")))
+  (defvar xwidget-webkit-search-word-prefix "https://duckduckgo.com/?q="
+    "Search engine url and search request that before searching word.")
+  (defvar xwidget-webkit-search-word-suffix "&kp=-1&kl=jp-jp"
+    "Search engine option parameter that after searching word.")
+  (defun xwidget-webkit-search-word (arg word)
+    "Searching from `xwidget-webkit-search-word-prefix' search engine.
 And searching with `xwidget-webkit-search-word-suffix' search engine options.
 Interactively, WORD defaults to the string looking like a word around point.
 If setting prefix args (C-u), reuses session(buffer). Normaly session(buffer) create."
-  (interactive (list
-                current-prefix-arg
-                (read-string
-                 "search-word: "
-                 (cond ((thing-at-point 'word))
-                       (t "")))))
-  (let ((encoded-url (url-encode-url
-                      (concat
-                       xwidget-webkit-search-word-prefix
-                       word
-                       xwidget-webkit-search-word-suffix))))
-    (xwidget-webkit-browse-url
-     encoded-url
-     (not arg))))
-
-;; (defun switch-buffer-functions-func (prev cur)
-;;   (if (string= "xwidget-webkit-mode"
-;;   	       (buffer-local-value 'major-mode
-;; 				   (get-buffer (or prev "*scratch*"))))
-;;     ))
-;; (add-hook 'switch-buffer-functions #'switch-buffer-functions-func)
+    (interactive (list
+                  current-prefix-arg
+                  (read-string
+                   "search-word: "
+                   (cond ((thing-at-point 'word))
+                         (t "")))))
+    (let ((encoded-url (url-encode-url
+                        (concat
+                         xwidget-webkit-search-word-prefix
+                         word
+                         xwidget-webkit-search-word-suffix))))
+      (xwidget-webkit-browse-url
+       encoded-url
+       (not arg)))))
 
 (when (eq system-type 'darwin)
   ;; enable bash
