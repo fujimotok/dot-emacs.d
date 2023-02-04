7dc62bd4e7ffe964791780541ca9c2207a82685f
fujimotok <fujimotok@users.noreply.github.com>
2023-02-05 00:09:02 +0900

    remove unused settings.

--- init.el
+++ init.el
@@ -885,12 +885,6 @@ mouse-1: Display Line and Column Mode Menu"
   :advice (:around eldoc-message
                    my:shutup-eldoc-message))
 
-(leaf paredit
-  :doc "lisp編集時の移動を楽にするパッケージ"
-  :ensure t
-  :hook ((emacs-lisp-mode-hook . paredit-mode)
-         (lisp-mode-hook . paredit-mode)))
-
 (leaf markdown-mode
   :doc "markdown用設定"
   :hook ((markdown-mode-hook . (lambda nil (outline-hide-sublevels 2))))
@@ -1258,9 +1252,6 @@ major-modeを一時的に親であるvue-modeに設定して、完了後戻す�
            'org-babel-load-languages
            '((plantuml . t))))
 
-(leaf crowi
-  :el-get hirocarma/emacs-crowi)
-
 (leaf auto-fix
   :el-get "tomoya/auto-fix.el"
   :hook ((auto-fix-mode-hook . setup-auto-fix))
@@ -1271,9 +1262,8 @@ major-modeを一時的に親であるvue-modeに設定して、完了後戻す�
      #'auto-fix-before-save)))
 
 ;;; Utilities
-(leaf hideshow
+(leaf *hideshow
   :doc "折り畳み機能のパッケージ"
-  :ensure t
   :hook ((c-mode-common-hook . hs-minor-mode)
          (emacs-lisp-mode-hook . hs-minor-mode)
          (java-mode-hook . hs-minor-mode)
@@ -1518,154 +1508,6 @@ major-modeを一時的に親であるvue-modeに設定して、完了後戻す�
   (defun google-translate--search-tkk ()
     "Search TKK."
     (list 430675 2721866130)))
-
-(leaf eww
-  :disabled t
-  :bind ((eww-mode-map
-          ("e" . ace-link-eww)))
-  :config ;;; デフォルトの設定(参考)
-  ;; (defun ace-link-setup-default ()
-  ;;   "Setup the defualt shortcuts."
-  ;;   (require 'info)
-  ;;   (define-key Info-mode-map "o" 'ace-link-info)
-  ;;   (require 'help-mode)
-  ;;   (define-key help-mode-map "o" 'ace-link-help)
-  ;;   (require 'eww)
-  ;;   (define-key eww-link-keymap "o" 'ace-link-eww)
-  ;;   (define-key eww-mode-map "o" 'ace-link-eww))
-  (ace-link-setup-default)
-  (defun ali--eww-collect-references nil
-    "Collect the positions of visible links in the current `eww' buffer."
-    (save-excursion
-      (save-restriction
-        (narrow-to-region
-         (window-start)
-         (window-end))
-        (goto-char (point-min))
-        (let ((skip (next-single-property-change
-                     (point)
-                     'help-echo))
-              candidates)
-          (while (setq skip
-                       (text-property-not-all
-                        skip
-                        (point-max)
-                        'help-echo
-                        nil))
-            (goto-char skip)
-            (push skip candidates)
-            (setq skip
-                  (next-single-property-change
-                   (point)
-                   'help-echo)))
-          (nreverse candidates))))))
-
-(leaf *xwidget-webkit
-  :disabled t
-  :hook ((xwidget-webkit-mode-hook . xwidget-webkit-mode-hook-func))
-  :config (defun xwidget-webkit-mode-hook-func nil
-            (remove-hook
-             'kill-buffer-query-functions
-             #'xwidget-kill-buffer-query-function)
-            (local-set-key
-             (kbd "C-f")
-             'xwidget-webkit-forward)
-            (local-set-key
-             (kbd "C-b")
-             'xwidget-webkit-back)
-            (local-set-key
-             (kbd "C-r")
-             'xwidget-webkit-reload)
-            (local-set-key
-             (kbd "C-l")
-             'xwidget-webkit-browse-url)
-            (local-set-key
-             (kbd "C-w")
-             'xwidget-webkit-current-url-message-kill)
-            (local-set-key
-             (kbd "C-q")
-             'kill-current-buffer)
-            (local-unset-key (kbd "C-x 2"))
-            (local-unset-key (kbd "C-x 3")))
-  (defvar xwidget-webkit-search-word-prefix "https://duckduckgo.com/?q="
-    "Search engine url and search request that before searching word.")
-  (defvar xwidget-webkit-search-word-suffix "&kp=-1&kl=jp-jp"
-    "Search engine option parameter that after searching word.")
-  (defun xwidget-webkit-search-word (arg word)
-    "Searching from `xwidget-webkit-search-word-prefix' search engine.
-And searching with `xwidget-webkit-search-word-suffix' search engine options.
-Interactively, WORD defaults to the string looking like a word around point.
-If setting prefix args (C-u), reuses session(buffer). Normaly session(buffer) create."
-    (interactive (list
-                  current-prefix-arg
-                  (read-string
-                   "search-word: "
-                   (cond ((thing-at-point 'word))
-                         (t "")))))
-    (let ((encoded-url (url-encode-url
-                        (concat
-                         xwidget-webkit-search-word-prefix
-                         word
-                         xwidget-webkit-search-word-suffix))))
-      (xwidget-webkit-browse-url
-       encoded-url
-       (not arg))))
-  ;; (defun switch-buffer-functions-func (prev cur)
-  ;;   (if (string= "xwidget-webkit-mode"
-  ;;   	       (buffer-local-value 'major-mode
-  ;; 				   (get-buffer (or prev "*scratch*"))))
-  ;;     ))
-  ;; (add-hook 'switch-buffer-functions #'switch-buffer-functions-func)
-  (add-hook
-   'xwidget-webkit-mode-hook
-   'xwidget-webkit-mode-hook-func)
-  (defun xwidget-webkit-mode-hook-func ()
-    (remove-hook
-     'kill-buffer-query-functions
-     #'xwidget-kill-buffer-query-function)
-    (local-set-key
-     (kbd "C-f")
-     'xwidget-webkit-forward)
-    (local-set-key
-     (kbd "C-b")
-     'xwidget-webkit-back)
-    (local-set-key
-     (kbd "C-r")
-     'xwidget-webkit-reload)
-    (local-set-key
-     (kbd "C-l")
-     'xwidget-webkit-browse-url)
-    (local-set-key
-     (kbd "C-w")
-     'xwidget-webkit-current-url-message-kill)
-    (local-set-key
-     (kbd "C-q")
-     'kill-current-buffer)
-    (local-unset-key (kbd "C-x 2"))
-    (local-unset-key (kbd "C-x 3")))
-  (defvar xwidget-webkit-search-word-prefix "https://duckduckgo.com/?q="
-    "Search engine url and search request that before searching word.")
-  (defvar xwidget-webkit-search-word-suffix "&kp=-1&kl=jp-jp"
-    "Search engine option parameter that after searching word.")
-  (defun xwidget-webkit-search-word (arg word)
-    "Searching from `xwidget-webkit-search-word-prefix' search engine.
-And searching with `xwidget-webkit-search-word-suffix' search engine options.
-Interactively, WORD defaults to the string looking like a word around point.
-If setting prefix args (C-u), reuses session(buffer). Normaly session(buffer) create."
-    (interactive (list
-                  current-prefix-arg
-                  (read-string
-                   "search-word: "
-                   (cond ((thing-at-point 'word))
-                         (t "")))))
-    (let ((encoded-url (url-encode-url
-                        (concat
-                         xwidget-webkit-search-word-prefix
-                         word
-                         xwidget-webkit-search-word-suffix))))
-      (xwidget-webkit-browse-url
-       encoded-url
-       (not arg)))))
 
 (leaf eaf
   :el-get emacs-eaf/emacs-application-framework
